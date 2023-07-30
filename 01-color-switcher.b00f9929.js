625d3c1a858e09521ebd8aef944adf8e084fcfba
AndyPolatovskyi <126195175+AndyPolatovskyi@users.noreply.github.com>
2023-07-30 23:14:55 +0000

    Deploying to gh-pages from @ AndyPolatovskyi/goit-js-hw-09@4f85b4e098728f71222314926d0423b6fc137c13 🚀

---
+++ 01-color-switcher.b00f9929.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d=null;t.addEventListener("click",(()=>{t.setAttribute("disabled",""),e.removeAttribute("disabled"),d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{t.removeAttribute("disabled"),e.setAttribute("disabled",""),clearInterval(d),console.log(`Interval with id ${d} has stopped!`)}));
+//# sourceMappingURL=01-color-switcher.b00f9929.js.map
